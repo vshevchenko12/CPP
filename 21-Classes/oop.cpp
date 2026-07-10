d7adb26e03c8438921e6a2e37a738a0dfdff1cd2
Vitalii Shevchenko <vshevchenko12@gmail.com>
2026-07-09 23:41:56 -0300

    added OOP

---
+++ 21-Classes/oop.cpp
@@ -0,0 +1,31 @@
+/*
+C++ What is OOP?
+OOP stands for Object-Oriented Programming.
+
+Object-oriented programming is about creating "objects", which can hold data and functions that work on that data.
+
+Advantages of OOP
+OOP provides a clear structure to programs
+Makes code easier to maintain, reuse, and debug
+Helps keep your code DRY (Don't Repeat Yourself)
+Makes it possible to create full reusable applications with less code and shorter development time
+Tip: The DRY principle means you should avoid writing the same code more than once. Move repeated code into functions or classes and reuse it.
+
+What are Classes and Objects?
+Classes and objects are the two main aspects of object-oriented programming.
+
+A class defines what an object should look like, and an object is created based on that class. For example:
+
+Class	Objects
+Fruit	Apple, Banana, Mango
+Car	Volvo, Audi, Toyota
+When you create an object from a class, it inherits all the variables and functions defined inside that class.
+
+
+Procedural vs Object-Oriented Programming
+Procedural programming is about writing functions that operate on data.
+
+Object-oriented programming (OOP) is about creating objects that contain both the data and the functions.
+
+In procedural programming, the code is organized around functions. In object-oriented programming, the code is organized around objects.
+*/
